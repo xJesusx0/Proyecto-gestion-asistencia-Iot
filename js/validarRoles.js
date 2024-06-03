35bf2efd95d564e69dc9a03f589720bb0705a258
jesus <jesuspl0411@gmail.com>
2024-06-02 22:15:30 -0500

    archivo adaptado al nuuevo sistema de rutas, ahora redirecciona a la ruta principal de cada rol

--- js/validarRoles.js
+++ js/validarRoles.js
@@ -4,9 +4,13 @@ const role = localStorage.getItem('role')
 console.log(route)
 console.log(window.routes[role])
 
-
-if (route !== window.routes[role]){
-    alert("No tienes permitido el acceso a esta pagina")
+if(!window.routes[role]){
+    alert("estas intentando acceder con un rol invalido") 
     localStorage.clear()
-    window.location.href = 'login.html'
+    window.location.href = 'login.html'   
+}
+
+if (!window.routes[role].includes(route)){
+    alert("No tienes permitido el acceso a esta pagina") 
+    window.location.href = window.routes[role][0]
 }
