10c1848257ec7414d25cdbfa2a357f7ee68c9f23
Daniel Dussan <daniel2014dussan@gmail.com>
2024-06-03 13:56:23 -0500

    Actualiacion de tarjtas con datos del Json y estilos arreglado

--- js/students.js
+++ js/students.js
@@ -21,10 +21,25 @@ fetch('../json/students.json')
     console.log(student['name'])
     document.getElementById('btnDesplegable').innerHTML = student['name']
     document.getElementById('welcomeUser').innerHTML += ` ${student['name']} 🌟`
-
-    //Ejemplos
-    //document.getElementById('cuatrimestre').innerHTML = student['period']
-    //document.getElementById('carrera').innerHTML = student['program']
+    document.getElementById('welcomeData').innerHTML += `${student['program']} <br> Cuatrimestre No ${student['period']} `
+    materias = student['courses']
+    
+    botonValido = ``
+   
+    
+    for (let j = 0; j < materias.length; j++) {
+      (materias[j].fails == 0) ? botonValido += `id="disabled-link" onclick="return false;"`: botonValido += ``
+      document.getElementById('pCards').innerHTML += `
+      <div class="card">
+        <img src="../img/textura.jpg" class="card-img-top" alt="Fondo Carta">
+        <div class="card-body">
+          <h5 class="card-title">${materias[j].course}</h5>
+          <p class="card-text">No de inasistencias: ${materias[j].fails}</p>
+          <a href="#" class="btnCard" ${botonValido}>Justificar</a>
+        </div>
+      </div>`
+      
+    }
 
   })
 
