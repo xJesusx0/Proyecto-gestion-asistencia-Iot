503dd734f0a83fde54f1f7c90c6f1c8747dbff15
jesus <jesuspl0411@gmail.com>
2024-06-13 18:11:45 -0500

    Se agregaron los diccionarios con las rutas locales y remotas

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,42 @@
 window.routes = {
-    "estudiante": [
-        "students.html"
+    'estudiante': [
+        'students.html'
     ],
-    "profesor": [
-        "teachers.html"
+    'profesor': [
+        'teachers.html'
     ],
-    "administrador": [
-        "administrators.html"
+    'administrador': [
+        'administrators.html'
     ]
 }
+
+const jsonLocalRoutes = {
+    'usersData': '../json/users_data.json',
+    'studentsData': '../json/students.json',
+    'teachersData': '../json/teachers.json',
+    'adminsData': '../json/administrators.json'
+}
+
+const jsonRemoteRoutes = {
+    'usersData': 'https://raw.githubusercontent.com/xJesusx0/Actividad-paginas-web/main/json/users_data.json',
+    'studentsData': 'https://raw.githubusercontent.com/xJesusx0/Actividad-paginas-web/main/json/students.json',
+    'teachersData': 'https://raw.githubusercontent.com/xJesusx0/Actividad-paginas-web/main/json/teachers.json',
+    'adminsData': 'https://raw.githubusercontent.com/xJesusx0/Actividad-paginas-web/main/json/administrators.json'
+}
+
+
+const getJsonRoutes = (environment = 'local') => {
+    if ( environment === 'local'){
+        return jsonLocalRoutes;
+    }
+
+    if (environment === 'remote'){
+        return jsonRemoteRoutes;
+    }
+};
+
+const ROUTES = getJsonRoutes('local'); //Cambiar esto a remote cuando se haga en main
+
+window.jsonRoutes = ROUTES; 
+
+// console.log(window.jsonRoutes);
