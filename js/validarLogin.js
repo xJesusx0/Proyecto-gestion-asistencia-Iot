0ec7ba8c69397a9505d6404151023d9a11e93a28
jesus <jesuspl0411@gmail.com>
2024-06-15 12:26:26 -0500

    se agrego un console.log

--- js/validarLogin.js
+++ js/validarLogin.js
@@ -6,7 +6,7 @@
 
 try {
     let loggedIn = userData.loggedIn;
-
+    console.log(loggedIn)
     if (loggedIn !== 'true') {
         localStorage.removeItem('userData');
         window.location.href = 'login.html';
