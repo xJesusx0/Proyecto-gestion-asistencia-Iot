4251934bf2b1c8822692399d0eb843838179ab6e
jesus <snowcatyt1234@gmail.com>
2024-06-25 21:34:24 -0500

    Archivos mejor organizados

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,4 +1,4 @@
-window.addEventListener('pageshow', function(event) {
+window.addEventListener('pageshow', function (event) {
   try {
     let storedData = localStorage.getItem('userData');
     let userData = JSON.parse(storedData);
@@ -8,63 +8,65 @@ window.addEventListener('pageshow', function(event) {
       let role = userData.role;
       window.location.href = window.routes[role][0];
     }
+
+    const loginForm = document.getElementById('login-form');
+    loginForm.setAttribute('action',`${config.baseUrl}/auth/login`)
+
   } catch (error) {
     console.error('Error al recuperar datos de localStorage:', error);
   }
 });
 
+const validarLogin = () => {
+  
+  const username = document.getElementById("username").value.toLowerCase().trim();
+  const password = document.getElementById("password").value;
+  const selectedRole = document.querySelector('input[name="role"]:checked').value;
+
+  // Verificar si los campos están vacíos
+  if (!username || !password || !selectedRole) {
+    alert('Por favor completa todos los campos.');
+    return;
+  }
 
 
-const validarDatos = () => {
+  // Datos que se enviaran en la peticion
+  const baseUrl = config.baseUrl
+  const url = `${baseUrl}/auth/login`
+  const data = {
+    'username': username,
+    'password': password,
+    'role': selectedRole
+  };
 
-  console.log(window.jsonRoutes)
+  console.log(selectedRole)
 
-  fetch(window.jsonRoutes.usersData)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('No se pudo leer el archivo JSON')
-      }
-      return response.json();
-    })
-
+  request('POST', url, data)
     .then(data => {
-      let username = document.getElementById("username").value.toLowerCase()
-      username = username.replace(/\s+/g, '')
+      console.log('Respuesta del servidor:', data);
 
-      const password = document.getElementById("password").value
-      const selectedRole = document.querySelector('input[name="role"]:checked').value
-
-      for (let i = 0; i < data.length; i++) {
-        let element = data[i];
-
-        if (username == element.username && password === element.password) {
-
-          if (selectedRole !== element.role) {
-            alert("Rol incorrecto")
-            return
-          }
-          window.userId = element.id;
-
-          alert(`Bienvenido de nuevo, ${username} 👋`)
-
-          const userData = {
-            'username': username,
-            'role': selectedRole,
-            'loggedIn': 'true',
-            'userId': element.id
-
-          }
-          localStorage.setItem('userData', JSON.stringify(userData))
-
-          window.location.href = window.routes[selectedRole][0]
-          return
-        }
+      if (data['error']) {
+        alert(data['error']);
+        return;
       }
 
-      alert("usuario o contraseña invalido")
 
+      const userData = {
+        'username': username,
+        'role': selectedRole,
+        'loggedIn': 'true',
+        'userId': data['id_usuario']
+
+      }
+
+      alert(`Bienvenido de nuevo, ${username} 👋`);
+
+      localStorage.setItem('userData', JSON.stringify(userData))
+      window.location.href = window.routes[selectedRole][0]
+      return
     })
+
     .catch(error => {
-      console.error('Error al leer el archivo JSON:', error)
+      console.error('Error:',error);
     });
 }
