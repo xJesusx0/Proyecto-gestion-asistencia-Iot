eed20c1cd215c4fb80a0734bc2d61a5cd8970f8c
jesus <snowcatyt1234@gmail.com>
2024-07-01 16:47:19 -0500

    cambios minimos

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,4 +1,4 @@
-window.addEventListener('pageshow', function(event) {
+window.addEventListener('pageshow', function (event) {
   try {
     let storedData = localStorage.getItem('userData');
     let userData = JSON.parse(storedData);
@@ -13,78 +13,34 @@ window.addEventListener('pageshow', function(event) {
   }
 });
 
-const csvTextToJson = (csvText) => {
-  // Eliminar cualquier caracter de retorno de carro adicional que pueda interferir
-  const formattedCsv = csvText.replace(/\r/g, '');
-
-  // Separar las líneas del CSV
-  const lines = formattedCsv.split('\n');
-
-  // Obtener los encabezados (primera línea del CSV)
-  const headers = lines[0].split(',');
-
-  const jsonArray = [];
-
-  // Recorrer cada línea del CSV (empezando desde la segunda línea)
-  for (let i = 1; i < lines.length; i++) {
-    const json = {};
-    const currentLine = lines[i].split(',');
-
-    // Asignar cada valor al encabezado correspondiente
-    for (let j = 0; j < headers.length; j++) {
-      json[headers[j]] = currentLine[j];
-    }
-
-    jsonArray.push(json);
-  }
-
-  return (jsonArray);
-}
-
 const validarDatos = () => {
+  request(window.jsonRoutes.usersData)
 
-  console.log(window.jsonRoutes)
-
-  fetch(window.jsonRoutes.usersData)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('No se pudo leer el archivo JSON')
-      }
-      return response.text();
-    })
-
-    .then(data => {
-      console.log(data)
-      const jsonData = csvTextToJson(data);
+    .then(usersData => {
       let correo = document.getElementById("username").value.toLowerCase()
       correo = correo.replace(/\s+/g, '')
 
       const contraseña = document.getElementById("password").value
-      const selectedRole = document.querySelector('input[name="role"]:checked').value
-      console.log(selectedRole)
-      for (let i = 0; i < jsonData.length; i++) {
-        let element = jsonData[i];
+      for (let i = 0; i < usersData.length; i++) {
+        let user = usersData[i];
 
-        if (correo == element.correo && contraseña === element.contraseña) {
+        if (correo == user.correo && contraseña === user.contraseña) {
 
-          if (selectedRole !== element.roles) {
-            alert("Rol incorrecto")
-            return
-          }
-          window.userId = element.id;
+          window.userId = user.id;
 
-          alert(`Bienvenido de nuevo, ${correo} 👋`)
+          alert(`Bienvenido de nuevo, ${user.nombres} 👋`)
 
           const userData = {
-            'correo': correo,
-            'role': selectedRole,
+            'nombres': user.nombres,
+            'apellido': user.apellidos,
+            'role': user.roles,
             'loggedIn': 'true',
-            'userId': element.id
-
+            'userId': user.id_usuario
           }
+
           localStorage.setItem('userData', JSON.stringify(userData))
 
-          window.location.href = window.routes[selectedRole][0]
+          window.location.href = window.routes[user.roles][0]
           return
         }
       }
@@ -93,6 +49,6 @@ const validarDatos = () => {
 
     })
     .catch(error => {
-      console.error('Error al leer el archivo JSON:', error)
+      console.error('Error al leer el archivo:', error)
     });
 }
