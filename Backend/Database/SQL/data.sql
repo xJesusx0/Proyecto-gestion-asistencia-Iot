6ec4f439e3d9e368c4ed883a5c7db59905591f6b
jesus <snowcatyt1234@gmail.com>
2024-07-03 18:26:17 -0500

    cambios menores

--- Backend/Database/SQL/data.sql
+++ Backend/Database/SQL/data.sql
@@ -9,5 +9,5 @@ insert into facultades(nombre,decano) values ('Ingenierias','Piedad Marchena Vil
 
 insert into programas (nombre,fkid_facultad,lugar_de_oferta,modalidad,duracion) values ('Técnica Profesional en Mantenimiento de Sistemas Informáticos',1,'Soledad','Presencial',5);
 
-insert into usuarios (id_usuario,correo,contraseña, nombres, apellidos,numero_telefonico) values (1234,'jesusperea@unibarranquilla.edu.co','jesus1234', 'Jesus Alberto', 'Perea Linares', '123456789');
+insert into usuarios (id_usuario,correo,contraseña, nombres, apellidos,numero_telefonico) values (1234,'jalbertoperea@unibarranquilla.edu.co','jesus1234', 'Jesus Alberto', 'Perea Linares', '123456789');
 insert into usuarios_roles(id_usuario,id_rol) values (1234,3);
--- Backend/Database/auth.py
+++ Backend/Database/auth.py
@@ -17,13 +17,7 @@ def validateLogin(mysql,cursor,user_data:dict):
 
 @handle_database_operations
 def get_roles(mysql,cursor,user_id:int):
-    # select id_usuario,nombre from usuarios_roles join roles on roles.id_rol where roles.id_rol = usuarios_roles.id_rol;
     cursor.execute('SELECT id_usuario,nombre FROM usuarios_roles JOIN roles ON roles.id_rol = usuarios_roles.id_rol WHERE usuarios_roles.id_usuario = %s ;',(user_id,))
 
     response = cursor.fetchall()
     return response
-
-#  SELECT roles.id_rol, roles.nombre
-#     FROM usuarios_roles
-#     JOIN roles ON roles.id_rol = usuarios_roles.id_rol
-#     WHERE usuarios_roles.id_usuario = %s
