9a8d8c4fca31e64f4304fa4e73f56c217cc98764
jesus <snowcatyt1234@gmail.com>
2024-07-05 21:46:05 -0500

    validar login ahora funciona con peticiones

--- Frontend/js/validarLogin.js
+++ Frontend/js/validarLogin.js
@@ -4,21 +4,20 @@
 //     window.location.href = 'login.html';
 // }
 window.addEventListener('pageshow', function (event) {
-    try {
-        let storedData = localStorage.getItem('userData');
-        let userData = JSON.parse(storedData);
-        let loggedIn = userData.loggedIn;
+        const url = `${config.baseUrl}/auth/validate-login`
+        request('GET',url)
+        .then(response => {  
+            let loggedIn = response['response'];
+            console.log(loggedIn)
+            if (loggedIn !== true) {
+                localStorage.removeItem('userData');
+                window.location.href = 'login.html';
+            }
+            console.log(loggedIn)
+        })
         
-        console.log(loggedIn)
-        if (loggedIn !== 'true') {
-            localStorage.removeItem('userData');
-            window.location.href = 'login.html';
-        }
 
-    } catch (error) {
-        localStorage.removeItem('userData');
-        window.location.href = 'login.html';
-    }
+   
 });
 
 
