983754e219a4775ea3387fa011bb10aefbf4774a
jesus <snowcatyt1234@gmail.com>
2024-07-05 21:47:19 -0500

    Nueva configuracion para el manejo de cookies y sesiones

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_session import Session
+from flask import make_response
 
 from flask_mysqldb import MySQL
 from app.config import Config
@@ -7,9 +9,12 @@ from app.config import Config
 from app.routes.auth import auth_bp
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-CORS(app)
+app.config.from_object(Config)
+app.secret_key = 'secret'
+
+Session(app)
+CORS(app,supports_credentials=True)
 mysql = MySQL(app)
 
 auth_bp.mysql = mysql
