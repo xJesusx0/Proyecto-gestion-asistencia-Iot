5326ceec3e524a32d1fadc974156b30d74fbb7fc
jesus <snowcatyt1234@gmail.com>
2024-07-06 16:57:18 -0500

    mejoras en las validaciones

--- Frontend/js/validarLogin.js
+++ Frontend/js/validarLogin.js
@@ -4,6 +4,12 @@
 //     window.location.href = 'login.html';
 // }
 window.addEventListener('pageshow', function (event) {
+
+    const localData = localStorage.getItem('userData');
+    if (!localData){
+        alert('Ha ocurrido un error con el local storage')
+        window.location.href = 'login.html';
+    }
         const url = `${config.baseUrl}/auth/validate-login`
         request('GET',url)
         .then(response => {  
