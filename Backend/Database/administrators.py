2fb55bb91dc0f7cb9d3b91b2d2d9d52c3ba3912a
jesus <snowcatyt1234@gmail.com>
2024-07-08 16:36:59 -0500

    se agregaron las querys parausuarios_roles

--- Backend/Database/administrators.py
+++ Backend/Database/administrators.py
@@ -3,9 +3,20 @@ from Database import handle_database_operations
 from Database.encrypt import encrypt
 
 @handle_database_operations
-def insert_by_csv(mysql,cursor,users_list:list):
-    print(users_list)
-    query = 'INSERT INTO usuarios (id_usuario,correo,contraseña, nombres, apellidos,numero_telefonico) VALUES (%s,%s,%s,%s,%s,%s)'
+def insert_by_csv(mysql,cursor,users_list:list,tablename:str):
+    print(tablename)
+
+    querys = {
+        'usuarios':'INSERT INTO usuarios (id_usuario,correo,contraseña, nombres, apellidos,numero_telefonico) VALUES (%s,%s,%s,%s,%s,%s)',
+        'estudiantes': 'INSERT INTO estudiante (id_estudiante, id_programa, cuatrimestre) VALUES (%s, %s, %s)',
+        'profesores': 'INSERT INTO profesor (id_profesor, especialidad) VALUES (%s, %s)',
+        'usuarios_roles': 'INSERT INTO usuarios_roles (id_usuario, id_rol) VALUES (%s, %s)'
+
+    }
+
+    query = querys.get(tablename,None)
+    if query == None:
+        raise ValueError('Nombre de tabla invalida')
     try:
         cursor.executemany(query,users_list)
     except Exception as e:
