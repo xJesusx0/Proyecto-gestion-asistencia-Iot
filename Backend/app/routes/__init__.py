f62c8c79a0d92aa69be6a91d239e884389c049f3
jesus <jesuspl0411@gmail.com>
2024-07-13 20:06:42 -0500

    Archivo que contiene las rutas web y de las apis

---
+++ Backend/app/routes/__init__.py
@@ -0,0 +1,39 @@
+web_routes = {
+    'estudiante': [
+        'students.html',
+        'students',
+        'history',
+        'history.html',
+        'justificacion',
+        'justificacion.html'
+    ],
+    'profesor': [
+        'teachers.html',
+        'teachers',
+        'students-list.html',
+        'students-list'
+    ],
+    'administrador': [
+        'administrators.html',
+        'administrators',
+        'add-student.html',
+        'add-student',
+        'upload-and-register',
+        'upload-and-register.html'
+    ]
+}
+
+api_routes = {
+    'estudiante': [
+        'get-modules'
+    ],
+    'profesor': [
+        ''
+    ],
+    'administrador': [
+        'administrators',
+        'add-student',
+        'upload-and-register-users',
+        'get-users',
+    ]
+}
