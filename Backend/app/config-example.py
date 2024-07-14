3d1951b6348e1a3460fa5e404ba5116c80b92307
jesus <jesuspl0411@gmail.com>
2024-07-13 20:05:15 -0500

    se agrego decoradores para proteger las rutas de las apis

--- Backend/app/config-example.py
+++ Backend/app/config-example.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from flask import request, jsonify
+from flask import request, jsonify,session
+from .routes import api_routes
 
 class Config:
     SESSION_PERMANENT = False
@@ -28,3 +29,33 @@ def token_required (func:callable):
         return func(*args, **kwargs)
     
     return wrapper
+
+def valid_login(func:callable):
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        if not session:
+            return jsonify({'error': 'No ha iniciado sesion'}), 401
+        
+        if not session['logged-in']:
+            return jsonify({'error': 'No ha iniciado sesion'}), 401
+        
+        return func(*args, **kwargs)
+
+    return wrapper
+
+def valid_role(route:str):
+    def decorator(func:callable):
+        @wraps(func)
+        def wrapper(*args,**kwargs):
+            if 'role' not in session or not session['role']:
+                return jsonify({'error': 'No ha iniciado sesion'}), 401
+
+            role = session.get('role')
+            routes_by_role = api_routes[role]
+            
+            if route not in routes_by_role:
+                return jsonify({'error':'esta ruta no esta disponible para tu rol'}),403
+
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
