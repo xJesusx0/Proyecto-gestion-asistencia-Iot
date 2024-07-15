80b580a2c7ac21e82cbacb435e9cd47a9178aee4
jesus <jesuspl0411@gmail.com>
2024-07-15 16:33:11 -0500

    se leen las rutas del json

--- Backend/app/routes/__init__.py
+++ Backend/app/routes/__init__.py
@@ -1,44 +1,26 @@
-web_routes = {
-    'estudiante': [
-        'students.html',
-        'students',
-        'history',
-        'history.html',
-        'justificacion',
-        'justificacion.html'
-    ],
-    'profesor': [
-        'teachers.html',
-        'teachers',
-        'students-list.html',
-        'students-list'
-    ],
-    'administrador': [
-        'administrators.html',
-        'administrators',
-        'add-student.html',
-        'add-student',
-        'upload-and-register',
-        'upload-and-register.html',
-        'users-list.html',
-        'users-list',
-        'user-info',
-        'user-info.html'
-    ]
-}
+import os
+import json
 
-api_routes = {
-    'estudiante': [
-        'get-modules'
-    ],
-    'profesor': [
-        ''
-    ],
-    'administrador': [
-        'administrators',
-        'add-student',
-        'upload-and-register-users',
-        'get-users',
-        'get-user-data',
-    ]
-}
+current_dir = os.path.dirname(__file__)
+
+web_routes_file = os.path.join(current_dir, '../../../json/web-routes.json')
+api_routes_file = os.path.join(current_dir, '../../../json/api-routes.json')
+
+web_routes_route = os.path.abspath(web_routes_file)
+api_routes_route = os.path.abspath(api_routes_file)
+
+web_routes = {}
+api_routes = {}
+
+if not os.path.exists(web_routes_route):
+    print(f"El archivo '{web_routes_file}' no existe.")
+
+if not os.path.exists(api_routes_route):
+    print(f"El archivo '{api_routes_file}' no existe.")
+        
+with open(web_routes_route, 'r') as f:
+    web_routes = json.load(f)
+
+with open(api_routes_route, 'r') as f:
+    api_routes = json.load(f)
+
