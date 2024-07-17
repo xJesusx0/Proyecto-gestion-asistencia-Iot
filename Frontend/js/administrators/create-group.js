fe519be6ffc38e1dd93ad6bc0f037daabb0c595e
jesus <jesuspl0411@gmail.com>
2024-07-16 21:31:48 -0500

    se agrego el archivo que agrega un grupo

--- Frontend/js/administrators/create-group.js
+++ Frontend/js/administrators/create-group.js
@@ -48,10 +48,32 @@ document.addEventListener('DOMContentLoaded',() => {
 });
 
 const createGroup = () => {
+    const groupId = document.getElementById('group-id').value;
+    const period = document.getElementById('period').value;
+    const weekday = document.getElementById('weekday').value;
     const selectedTeacher = document.getElementById('teachers').value;
     const selectedModule = document.getElementById('modules').value;
     const selectedClassroom = document.getElementById('classrooms').value;
+    const startTime = document.getElementById('start-time').value;
+    const endTime = document.getElementById('end-time').value;
 
-    console.log(selectedClassroom,selectedModule,selectedTeacher)
+    const data = {
+        'group-id':groupId,
+        'module':selectedModule,
+        'teacher':selectedTeacher,
+        'classroom':selectedClassroom,
+        'period':period,
+        'weekday':weekday,
+        'start-time':startTime,
+        'end-time':endTime
+    };
 
+    const url = `${config.baseUrl}/admin/add-group`
+
+    console.log(data)
+
+    request('POST',url,data)
+    .then(response => {
+        console.log(response)
+    })
 }
